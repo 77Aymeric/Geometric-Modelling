cdeed58cdad4e05205bda9538e1120b3c70ee3ee
Aymeric <aymeric.letellier@edu.esiee.fr>
2026-04-05 19:01:26 +0200

    Premier ComputeNormal

--- myFace.cpp
+++ myFace.cpp
@@ -17,5 +17,16 @@ myFace::~myFace(void)
 
 void myFace::computeNormal()
 {
-	/**** TODO ****/
+	/**** TODO ****/ //inspiré de ce qu'il y a avant correction
+	myHalfedge *h = adjacent_halfedge;
+	myVector3D v1, v2;
+	v1.dX = h->source->point->X - h->next->source->point->X;
+	v1.dY = h->source->point->Y - h->next->source->point->Y;
+	v1.dZ = h->source->point->Z - h->next->source->point->Z;
+	h = h->next;
+	v2.dX = h->source->point->X - h->next->source->point->X;
+	v2.dY = h->source->point->Y - h->next->source->point->Y;
+	v2.dZ = h->source->point->Z - h->next->source->point->Z;
+	normal->crossproduct(v1, v2);
+	normal->normalize();
 }
